ee1e0e37bb6e69d36f146fe99bd01430ce139122
Miguel Montoya <mimontoy@yahoo.com>
2023-05-08 17:25:20 +0000

    chapter 1 end task jaffle shop

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,17 +1,5 @@
-with source as (
-
-    select * from raw.jaffle_shop.customers
-
-),
-
-staged as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-    from source
-
-)
-
-select * from staged
+select 
+    id as customer_id,
+    first_name,
+    last_name
+from {{ source('jaffle_shop', 'customers') }}
